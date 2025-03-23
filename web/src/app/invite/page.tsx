0ce36285abc5e48fb4f72f4dec42dfafe6c39a1a
vandodev <vandoaparecido@hotmail.com>
2025-03-23 07:35:34 -0300

    :rocket: :dollar: :white_check_mark: Separando ranking em componente

--- web/src/app/invite/page.tsx
+++ web/src/app/invite/page.tsx
@@ -3,10 +3,8 @@ import logo from '../../assets/logo.svg'
 import { InputField, InputIcon, InputRoot } from '../components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import { IconButton } from '../components/icon-button'
+import { Ranking } from './ranking'
 
-import cooper from '../../assets/medal-cooper.svg'
-import gold from '../../assets/medal-gold.svg'
-import silver from '../../assets/medal-silver.svg'
 
 export default async function InvitePage() {
     return(
@@ -80,56 +78,7 @@ export default async function InvitePage() {
                 </div>
             </div>
 
-            <div className="w-full max-w-[440px] space-y-5">
-                <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-                    Ranking de indicações
-                </h2>
-
-                <div className="space-y-4">
-                    <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                        <span className="text-sm text-gray-300 leading-none">
-                            <span className="font-semibold">2 &ordm;</span> {" "}
-                            Ana carolina
-                        </span>
-
-                        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                            156585
-                        </span>
-                        
-                        <Image src={gold} alt="" className="absolute top-0 right-8" />
-                    </div>
-
-                    <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                        <span className="text-sm text-gray-300 leading-none">
-                            <span className="font-semibold">1 &ordm;</span> {" "}
-                            Evando Oliveira
-                        </span>
-
-                        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                            12565
-                        </span>
-                        
-                        <Image src={silver} alt="" className="absolute top-0 right-8" /> 
-                    </div>
-
-                    <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                        <span className="text-sm text-gray-300 leading-none">
-                            <span className="font-semibold">3 &ordm;</span> {" "}
-                            Israel Oliveira
-                        </span>
-
-                        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                            12565
-                        </span>
-                        
-                        <Image src={cooper} alt="" className="absolute top-0 right-8" /> 
-                    </div>
-                   
-                        
-                </div> 
-                    
-                   
-            </div>
+            <Ranking />
             
         </div>
     )
