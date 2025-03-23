4ae2c3c788d3c99eebee0a884aa5b1927a24d9d6
vandodev <vandoaparecido@hotmail.com>
2025-03-23 07:43:49 -0300

    :rocket: :dollar: :white_check_mark: Separando stats em componente

--- web/src/app/invite/page.tsx
+++ web/src/app/invite/page.tsx
@@ -4,6 +4,7 @@ import { InputField, InputIcon, InputRoot } from '../components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import { IconButton } from '../components/icon-button'
 import { Ranking } from './ranking'
+import { Stats } from './stats'
 
 
 export default async function InvitePage() {
@@ -43,38 +44,7 @@ export default async function InvitePage() {
                         </IconButton>
                     </InputRoot>
 
-                    <div className="grid md:grid-cols-3 gap-3">
-                        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-                            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                                1067
-                            </span>
-
-                            <span className="text-sm text-gray-300 leading-none text-center">
-                                Acessos ao link
-                            </span>
-                            <MousePointerClick className="size-5 text-purple top-3 left-3 absolute" />
-                        </div>
-                        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-                            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                                1067
-                            </span>
-                            
-                            <span className="text-sm text-gray-300 leading-none text-center">
-                                Inscrições feitas
-                            </span>
-                            <BadgeCheck className="size-5 text-purple top-3 left-3 absolute" />
-                        </div>
-                        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-                            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                                10
-                            </span>
-                            
-                            <span className="text-sm text-gray-300 leading-none text-center">
-                                Posição no ranking
-                            </span>
-                            <Medal className="size-5 text-purple top-3 left-3 absolute" />
-                        </div>
-                    </div>
+                  <Stats />
                 </div>
             </div>
 
