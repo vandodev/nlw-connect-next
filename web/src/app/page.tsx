d219708d4307c83ca85c9dc2bb3d68c254e58b32
vandodev <vandoaparecido@hotmail.com>
2025-03-24 07:11:35 -0300

    :rocket: :dollar: :white_check_mark: Separando formulário de inscrição em componente

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,6 +3,7 @@ import logo from '../assets/logo.svg'
 import { ArrowRight, Mail, Radio, User } from 'lucide-react'
 import { InputField, InputIcon, InputRoot } from './components/input';
 import { Button } from './components/button';
+import { SubscriptionForm } from './subscription-form';
 
 export default function Home() {
   return (
@@ -39,32 +40,7 @@ export default function Home() {
           </p>
         </div>
 
-        <form className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="text" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRight className="size-6" />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
     
