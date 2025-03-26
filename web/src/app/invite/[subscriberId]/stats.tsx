12b4c5ce3c514021713ab6ec4b04ece8840fe0c7
vandodev <vandoaparecido@hotmail.com>
2025-03-26 07:47:44 -0300

    :rocket: :dollar: :white_check_mark: Mostrando quantidade de acesso ao link

--- web/src/app/invite/[subscriberId]/stats.tsx
+++ web/src/app/invite/[subscriberId]/stats.tsx
@@ -1,38 +1,44 @@
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+import {getSubscriberInviteClicks} from '@/http/api'
 
-export function Stats() {
+interface StatsProps {
+    subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+    const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
     return(
         <div className="grid md:grid-cols-3 gap-3">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1067
-            </span>
+            <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+                <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                    {accessCount}
+                </span>
 
-            <span className="text-sm text-gray-300 leading-none text-center">
-                Acessos ao link
-            </span>
-            <MousePointerClick className="size-5 text-purple top-3 left-3 absolute" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1067
-            </span>
-            
-            <span className="text-sm text-gray-300 leading-none text-center">
-                Inscrições feitas
-            </span>
-            <BadgeCheck className="size-5 text-purple top-3 left-3 absolute" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                10
-            </span>
-            
-            <span className="text-sm text-gray-300 leading-none text-center">
-                Posição no ranking
-            </span>
-            <Medal className="size-5 text-purple top-3 left-3 absolute" />
-        </div>
+                <span className="text-sm text-gray-300 leading-none text-center">
+                    Acessos ao link
+                </span>
+                <MousePointerClick className="size-5 text-purple top-3 left-3 absolute" />
+            </div>
+            <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+                <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                    1067
+                </span>
+                
+                <span className="text-sm text-gray-300 leading-none text-center">
+                    Inscrições feitas
+                </span>
+                <BadgeCheck className="size-5 text-purple top-3 left-3 absolute" />
+            </div>
+            <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+                <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                    10
+                </span>
+                
+                <span className="text-sm text-gray-300 leading-none text-center">
+                    Posição no ranking
+                </span>
+                <Medal className="size-5 text-purple top-3 left-3 absolute" />
+            </div>
         </div>
     )
 }
