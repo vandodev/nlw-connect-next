398e1a6a2e15be1b82c6c75fa1cc864d8b70d148
vandodev <vandoaparecido@hotmail.com>
2025-03-26 08:27:24 -0300

    :rocket: :dollar: :white_check_mark: Mostrando quantidade de inscrição concretizada

--- web/src/app/invite/[subscriberId]/stats.tsx
+++ web/src/app/invite/[subscriberId]/stats.tsx
@@ -1,5 +1,5 @@
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
-import {getSubscriberInviteClicks} from '@/http/api'
+import { getSubscriberInviteClicks, getSubscriberInviteCount} from '@/http/api'
 
 interface StatsProps {
     subscriberId: string
@@ -7,6 +7,7 @@ interface StatsProps {
 
 export async function Stats({ subscriberId }: StatsProps) {
     const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
+    const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
     return(
         <div className="grid md:grid-cols-3 gap-3">
             <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
@@ -21,7 +22,7 @@ export async function Stats({ subscriberId }: StatsProps) {
             </div>
             <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
                 <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                    1067
+                    {inviteCount}
                 </span>
                 
                 <span className="text-sm text-gray-300 leading-none text-center">
