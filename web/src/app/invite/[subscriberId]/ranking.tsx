d510738ef9163a2f77dd89069a1fac2636123116
vandodev <vandoaparecido@hotmail.com>
2025-03-26 08:51:43 -0300

    :rocket: :dollar: :white_check_mark: Mostrando ranking de indicações

--- web/src/app/invite/[subscriberId]/ranking.tsx
+++ web/src/app/invite/[subscriberId]/ranking.tsx
@@ -3,7 +3,10 @@ import cooper from '../../../assets/medal-cooper.svg'
 import gold from '../../../assets/medal-gold.svg'
 import silver from '../../../assets/medal-silver.svg'
 
+import { getRanking } from '@/http/api'
+
 export async function Ranking() {
+    const { ranking } = await getRanking()
     return(
         <div className="w-full max-w-[440px] space-y-5">
             <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -11,44 +14,37 @@ export async function Ranking() {
             </h2>
 
             <div className="space-y-4">
-                <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                    <span className="text-sm text-gray-300 leading-none">
-                        <span className="font-semibold">2 &ordm;</span> {" "}
-                        Ana carolina
-                    </span>
+             {ranking.map((rank, index) => {
+            const rankingPosition = index + 1
 
-                    <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                        156585
-                    </span>
-                    
-                    <Image src={gold} alt="" className="absolute top-0 right-8" />
-                </div>
+                return (
+                    <div
+                        key={rank.id}
+                        className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+                    >
+                        <span className="text-sm text-gray-300 leading-none">
+                            <span className="font-semibold">{rankingPosition}&ordm;</span> |{' '}
+                            {rank.name}
+                        </span>
 
-                <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                    <span className="text-sm text-gray-300 leading-none">
-                        <span className="font-semibold">1 &ordm;</span> {" "}
-                        Evando Oliveira
-                    </span>
+                        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                            {rank.score}
+                        </span>
 
-                    <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                        12565
-                    </span>
-                    
-                    <Image src={silver} alt="" className="absolute top-0 right-8" /> 
-                </div>
+                        {rankingPosition === 1 && (
+                            <Image src={gold} alt="" className="absolute top-0 right-8" />
+                        )}
 
-                <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-                    <span className="text-sm text-gray-300 leading-none">
-                        <span className="font-semibold">3 &ordm;</span> {" "}
-                        Israel Oliveira
-                    </span>
+                        {rankingPosition === 2 && (
+                            <Image src={silver} alt="" className="absolute top-0 right-8" />
+                        )}
 
-                    <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                        12565
-                    </span>
-                    
-                    <Image src={cooper} alt="" className="absolute top-0 right-8" /> 
-                </div>                
+                        {rankingPosition === 3 && (
+                            <Image src={cooper} alt="" className="absolute top-0 right-8" />
+                        )}
+                    </div>
+                )
+            })}                             
                     
             </div>                  
                    
